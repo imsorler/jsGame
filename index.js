f7d4cf574e5d98e364f7fc09a55589a9f84eac30
eugene <imsorler@gmail.com>
2020-11-19 11:18:30 +0300

    some fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const $start = document.querySelector('#start')
 const $game = document.querySelector('#game')
 
-const score = 0
+let score = 0
 
 $start.addEventListener('click', startGame)
 $game.addEventListener('click', handleBoxClick)
@@ -14,23 +14,34 @@ function startGame() {
 }
 
 function handleBoxClick(event) {
-  if (event.target.dataset) {
+  if (event.target.dataset.box) {
     score++
     renderBox()
   }
 }
 
 function renderBox() {
-  $game.innerHTML = ''
+  $game.innerHTML = ' '
   const box =  document.createElement('div')
+
+  let boxSize = getRandom(30, 100)
+  let gameSize = $game.getBoundingClientRect()
+  let maxTop = gameSize.height - boxSize
+  let maxLeft = gameSize.width - boxSize
+  console.log(gameSize)
   
-  box.style.height = box.style.width = '50px'
+  box.style.height = box.style.width = `${boxSize}px`
   box.style.position = 'absolute'
   box.style.backgroundColor = '#000'
-  box.style.top = '50px'
-  box.style.left = '70px'
+  box.style.top = getRandom(0, maxTop) + 'px'
+  box.style.left = getRandom(0, maxLeft) + 'px'
   box.style.cursor = 'pointer'
   box.setAttribute('data-box', 'true')
 
   $game.insertAdjacentElement('afterbegin', box)
+}
+
+function getRandom(min, max) {
+  let num = min - 0.5 + Math.random() * (max - min + 1)
+  return Math.round(num)
 }
